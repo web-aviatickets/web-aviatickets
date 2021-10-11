d6d7ff068e5cf5e87f0d14c57b99dce68a9f279b
liza <gorbunova.elizaveta@gmail.com>
2021-10-11 23:25:35 +0300

    add media queries

--- src/frontJS/main.js
+++ src/frontJS/main.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //const img = document.getElementById('form-container');
+  //img.style.width = window.innerWidth;
+  //img.style.height = window.innerHeight;
   styleFooter();
 });
