eab4163dcdb97bca77929532296139d1dad66018
liza <gorbunova.elizaveta@gmail.com>
2021-10-12 11:59:40 +0300

    update mq

--- src/frontJS/footer.js
+++ src/frontJS/footer.js
@@ -13,10 +13,10 @@ function styleFooter() {
     </div>
     <div class="references-container">
       <div class="social-networks">
-        <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/>
-        <img src="https://img.icons8.com/color/48/000000/telegram-app--v5.png"/>
-        <img src="https://img.icons8.com/color/48/000000/youtube-play.png"/>
-        <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png"/>
+        <a href="https://www.instagram.com/"><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png" /></a>
+        <a href="https://web.telegram.org/k/"><img src="https://img.icons8.com/color/48/000000/telegram-app--v5.png" /></a>
+        <a href="https://www.youtube.com/"><img src="https://img.icons8.com/color/48/000000/youtube-play.png" /></a>
+        <a href="https://uk-ua.facebook.com/"><img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" /></a>
       </div>
       <p>(044) 111-11-11
       <br>
