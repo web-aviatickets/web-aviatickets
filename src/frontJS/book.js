0fe41876354eba837050ae60b31146eb88e143f0
Bespaliy <55046429+Bespaliy@users.noreply.github.com>
2021-11-29 16:43:38 +0200

    add front

--- src/frontJS/book.js
+++ src/frontJS/book.js
@@ -1,2 +1,8 @@
-const place = document.querySelector('.place');
-console.dir(place);
+const places = document.querySelector('.places_choose_board_cabin');
+
+places.addEventListener('click', e => {
+    e.preventDefault();
+    const place = e.target.closest('.place');
+    console.log(e);
+    if (place.classList.contains('white')) place.classList.toggle('red');
+});
