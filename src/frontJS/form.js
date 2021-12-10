11293bbdc397b2fc0b104becba6b0909734fbc64
Bespaliy <55046429+Bespaliy@users.noreply.github.com>
2021-12-10 14:41:44 +0200

    add cookies

--- src/frontJS/form.js
+++ src/frontJS/form.js
@@ -5,8 +5,6 @@ const mounths = document.querySelector('.mounth_container');
 const inpMounth = document.getElementById('input_mounth');
 const arrows = document.querySelector('.pasengers_data');
 
-console.log(document.cookie.path);
-
 const buildApi = (methods) => {
     const api = {};
     for (const method of methods) {
@@ -43,31 +41,34 @@ form.addEventListener('submit', e => {
     const json = JSON.stringify(Object.fromEntries(formData.entries()));
     api[method](json)
         .then(data => {
-            document.cookie = "user=Jhon";
-        })
-        .then(window.location.href = `${pathTo}`);
+            document.cookie = 'Jhio=dsf'
+            console.log(data, document.cookie);
+            window.location.href = `${pathTo}`
+            return;
+        });
+       //.then(window.location.href = `${pathTo}`);
 
 });
 
-arrows.addEventListener('click', e => {
-    e.preventDefault();
-    const arrow = e.target.closest('.arr');
-    const nextArrow = arrow.nextElementSibling;
-    const previousArrow = arrow.previousElementSibling;
-    if (nextArrow) {
-        nextArrow.classList.remove('hidden');
-        arrow.classList.add('hidden');
-    } else {
-        previousArrow.classList.remove('hidden');
-        arrow.classList.add('hidden');
-    }
-    mounths.classList.toggle('hidden');
-});
+// arrows.addEventListener('click', e => {
+//     e.preventDefault();
+//     const arrow = e.target.closest('.arr');
+//     const nextArrow = arrow.nextElementSibling;
+//     const previousArrow = arrow.previousElementSibling;
+//     if (nextArrow) {
+//         nextArrow.classList.remove('hidden');
+//         arrow.classList.add('hidden');
+//     } else {
+//         previousArrow.classList.remove('hidden');
+//         arrow.classList.add('hidden');
+//     }
+//     mounths.classList.toggle('hidden');
+// });
 
-mounths.addEventListener('click', e => {
-    e.preventDefault();
-    const mounth = e.target.closest('.mounth');
-    const text = mounth.textContent;
-    inpMounth.value = text;
-    mounths.classList.add('hidden');
-});
+// mounths.addEventListener('click', e => {
+//     e.preventDefault();
+//     const mounth = e.target.closest('.mounth');
+//     const text = mounth.textContent;
+//     inpMounth.value = text;
+//     mounths.classList.add('hidden');
+// });
