4a579c07ca4f38d609ea50ea5e6a15b742a7d36c
NikitaLitvishko <litvishko.nikita@mail.ru>
2021-12-15 13:01:42 +0200

    choosing only one seet

--- src/frontJS/book.js
+++ src/frontJS/book.js
@@ -1,7 +1,23 @@
 const places = document.querySelector('.places_choose_board_cabin');
+const arr = [];
 
 places.addEventListener('click', e => {
     e.preventDefault();
     const place = e.target.closest('.place');
-    if (place.classList.contains('white')) place.classList.toggle('red');
+    choise_place(place);
 });
+
+function choise_place(place) {
+  arr.push(place);
+  if (arr.length == 1) {
+    if (place.classList.contains('white')) place.classList.toggle('red');
+  }
+  if (arr.length > 1) {
+    for(let i = 0; i < arr.length - 1; i++) {
+      if (arr[i].classList.contains('red')) arr[i].classList.toggle('red');
+    }
+    if (arr[arr.length-1].classList.contains('white')) {
+      place.classList.toggle('red');
+    }
+  }
+}
