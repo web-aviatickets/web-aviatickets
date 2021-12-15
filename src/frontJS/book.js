c915044731e68b0f87f064ee3efff1f7552b91af
NikitaLitvishko <litvishko.nikita@mail.ru>
2021-12-15 13:04:55 +0200

    less code

--- src/frontJS/book.js
+++ src/frontJS/book.js
@@ -4,20 +4,14 @@ const arr = [];
 places.addEventListener('click', e => {
     e.preventDefault();
     const place = e.target.closest('.place');
-    choise_place(place);
+    const choosed_place = choise_place(place);
 });
 
 function choise_place(place) {
   arr.push(place);
-  if (arr.length == 1) {
-    if (place.classList.contains('white')) place.classList.toggle('red');
+  for(let i = 0; i < arr.length - 1; i++) {
+    if (arr[i].classList.contains('red')) arr[i].classList.toggle('red');
   }
-  if (arr.length > 1) {
-    for(let i = 0; i < arr.length - 1; i++) {
-      if (arr[i].classList.contains('red')) arr[i].classList.toggle('red');
-    }
-    if (arr[arr.length-1].classList.contains('white')) {
-      place.classList.toggle('red');
-    }
-  }
+  if (arr[arr.length-1].classList.contains('white')) place.classList.toggle('red');
+  return arr[arr.length-1];
 }
